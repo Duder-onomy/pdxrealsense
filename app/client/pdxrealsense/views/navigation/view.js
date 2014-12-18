01b87d477a8dc03ac3ac26b85983d659103bb32f
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-12-18 12:03:32 -0800

    Empty staging.

--- app/client/pdxrealsense/views/navigation/view.js
+++
@@ -1,8 +0,0 @@
-/*global define:false*/
-define(['masseuse', './options'], function(masseuse, options) {
-    'use strict';
-
-    return masseuse.plugins.rivets.RivetsView.extend({
-        defaultOptions : options
-    });
-});
